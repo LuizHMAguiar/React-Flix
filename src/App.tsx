d9839f472ce43eba87328251ff3c5dec346541ce
LuizHMAguiar <1397749@etfbsb.edu.br>
2025-10-31 09:59:28 -0300

    Implementação do sistema de favoritos com Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import MovieDetails from './pages/MovieDetails';
+import { FavoritesProvider } from './contexts/FavoritesContext';
 
 export default function App() {
   return (
-    <Router>
-      <h1>React Flix</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<MovieDetails />} />
-        <Route path="*" element={<div>Página não encontrada</div>} />
-      </Routes>
-    </Router>
+    <FavoritesProvider>
+      <Router>
+        <h1>React Flix</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<MovieDetails />} />
+          <Route path="*" element={<div>Página não encontrada</div>} />
+        </Routes>
+      </Router>
+    </FavoritesProvider>
   );
 }
--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -2,10 +2,16 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMovieDetails } from '../services/api';
 import { type Movie } from '../types';
+import { useFavorites } from '../contexts/FavoritesContext';
 
 export default function MovieDetails() {
   const { id } = useParams();
   const [movie, setMovie] = useState<Movie>();
+
+  const { favorites, addFavorite, removeFavorite } = useFavorites();
+  let isFavorite=false;
+  if (movie)
+    isFavorite = favorites.some(fav => fav.imdbID === movie.imdbID);
 
   useEffect(() => {
     const fetchMovie = async () => {
@@ -26,5 +32,9 @@ export default function MovieDetails() {
       <p><strong>Ano:</strong> {movie.Year}</p>
       <p><strong>Diretor:</strong> {movie.Director}</p>
       <p><strong>Enredo:</strong> {movie.Plot}</p>
+      <button
+        onClick={() => isFavorite ? removeFavorite(movie.imdbID) : addFavorite(movie)}>
+        {isFavorite ? '★' : '☆'}
+      </button>
     </div>);
 }
